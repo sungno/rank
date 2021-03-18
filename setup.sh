4a8fdbedcbc2b00f853065da3359630001e35e4a
sungno <ysn3939@gmail.com>
2021-03-18 11:03:34 +0900

    Procfile , setup.sh edit

--- setup.sh
+++ setup.sh
@@ -1,8 +1,19 @@
 mkdir -p ~/.streamlit/
 
+echo "\
+[general]\n\
+email = \"ysn3939@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+
+
 echo “\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 “ > ~/.streamlit/config.toml
+
+
+
+
